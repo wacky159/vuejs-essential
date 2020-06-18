353781539549362e403f6cabf59f583bf9c84a08
henry <henry@gmail.com>
2020-06-18 16:09:24 +0800

    网站LOGO

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+  data() {
+    return {
+      logo: {
+        src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
+        title: "Learnku Vue.js"
+      }
+    };
+  },
+  beforeCreate() {
+    this.uploadsUrl = "https://learnku.com/vuejs/uploads/";
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  display: none;
+}
+</style>
